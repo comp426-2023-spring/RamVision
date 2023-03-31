b63bc6664b54f5c37f42f7d901bc8de58ead62b4
Aayush Singh <aayushkt@gmail.com>
2023-03-31 16:46:28 -0400

    HTTP url to database setup for posting

--- RamVision/src/app/submissionform/submissionform.component.ts
+++ RamVision/src/app/submissionform/submissionform.component.ts
@@ -28,5 +28,14 @@ export class SubmissionFormComponent {
     
     // Do something with the submission data
     console.log(submissionData);
+ 	
+    // in order to update the database we make the HTTP PUT call:
+    //'https://ramvision-ecaa0-default-rtdb.firebaseio.com/{year}/{academicterm}/{class}/{professor}/.json'
+    // with the following json in the data field of the PUT:
+    // { "grade" : gradeReceived}
+    // where gradeReceived is replaced with the actual data value, and so are {year}/{class}/... etc.  
+    const urlString = 'https://ramvision-ecaa0-default-rtdb.firebase.io.com/' + submissionData.year + '/' + submissionData.academicTerm + '/' + submissionData.class + '/' + submissionData.professor + '/.json'
+    
+    //still working on posting the grade to the above url
   }
 }
