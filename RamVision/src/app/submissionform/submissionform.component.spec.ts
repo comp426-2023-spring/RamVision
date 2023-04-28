541a60a6027046ecce8074fc3d046ec668fd3ead
pradyunramesh <pradyun.ramesh@gmail.com>
2023-04-28 15:47:25 -0400

    Error corrections

--- RamVision/src/app/submissionform/submissionform.component.spec.ts
+++ RamVision/src/app/submissionform/submissionform.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 import { SubmissionFormComponent } from './submissionform.component';
 
 describe('SubmissionformComponent', () => {
@@ -8,6 +9,7 @@ describe('SubmissionformComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, HttpClientModule], 
       declarations: [ SubmissionFormComponent ]
     })
     .compileComponents();
